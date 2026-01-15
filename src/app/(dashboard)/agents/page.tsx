81754c1f248e6654e8c775b89523374bbbcdb558
qkransrb <qkransrb90@gmail.com>
2026-01-15 16:23:29 +0900

    09: agents setup

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import {
+  AgentsView,
+  AgentViewError,
+  AgentViewLoading,
+} from "@/modules/agents/ui/views/agents-view";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentViewLoading />}>
+        <ErrorBoundary fallback={<AgentViewError />}>
+          <AgentsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
