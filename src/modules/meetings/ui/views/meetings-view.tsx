eb66166f5636e31418c9e0cf2ac018cc3ed32b6e
qkransrb <qkransrb90@gmail.com>
2026-01-16 15:37:11 +0900

    16: meetings setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+
+export const MeetingsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="This may take a few seconds..."
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Meetings"
+      description="Please try again later"
+    />
+  );
+};
